6c6a4e452c7c9aaf77a77f06393db9c58024f41d
Vitor Andrade dos Santos <mp8andrade@hotmail.com>
2017-06-27 12:04:27 -0300

    Adicionada atividade para conexao com servidor, ainda nao concluida

---
+++ app/src/main/java/com/lab309/computerRemote/ConnectActivity.java
@@ -0,0 +1,15 @@
+package com.lab309.computerRemote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lab309.computercontrol.R;
+
+public class ConnectActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_connect);
+	}
+}
